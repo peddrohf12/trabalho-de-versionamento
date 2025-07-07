61bf8cdb76eb9ff4553edf2c1c5692497825fbcb
Pedro Henrique <pedrohenriquefurtadocosta@gmail.com>
2025-07-06 21:42:34 -0300

    Estruturação da Página de Eventos

--- src/app/eventos/page.jsx
+++ src/app/eventos/page.jsx
@@ -1,12 +1,28 @@
+import { Card } from "@/components/ui/card";
+
 export default function PaginaEventos() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>
-          Seja bem-vindo a Página de Eventos do Trabalho de Versionamento de Software!
-          <br />
-        </h1>
-      </main>
+      <div className="flex flex-col bg-gray-200 h-screen w-screen justify-center items-center">
+        <div id="box" className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-bold text-gray-800">
+            Bem vindo a página de eventos!
+          </h1>
+
+          <Card className={"w-120 p-6 mt-4 bg-white shadow-md"}>
+            <h1>Evento 1</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus inventore corrupti veritatis deserunt </p>
+          </Card>
+          <Card className={"w-120 p-6 mt-4 bg-white shadow-md"}>
+            <h1>Evento 2</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus inventore corrupti veritatis deserunt </p>
+          </Card>
+          <Card className={"w-120 p-6 mt-4 bg-white shadow-md"}>
+            <h1>Evento 3</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus inventore corrupti veritatis deserunt </p>
+          </Card>
+        </div>
+      </div>
     </>
   );
 }
