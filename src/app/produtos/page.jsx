a84e6765c50480c3497bf0410210600e16568fbf
Pedro Henrique <pedrohenriquefurtadocosta@gmail.com>
2025-07-06 21:47:11 -0300

    Estruturação da Página de Produtos

--- src/app/produtos/page.jsx
+++ src/app/produtos/page.jsx
@@ -1,12 +1,34 @@
+import { Card } from "@/components/ui/card";
+
 export default function PaginaProdutos() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>
-          Seja bem-vindo a Página de Produtos do Trabalho de Versionamento de Software!
-          <br />
-        </h1>
-      </main>
+      <div className="flex flex-col justify-center items-center w-screen h-screen bg-gray-200">
+        <div id="box" className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-bold text-gray-800">
+            Bem vindo a página de produtos!
+          </h1>
+
+          <Card className={"w-96 p-6 mt-4 bg-white shadow-md"}>
+            <h1>
+              Produto 1
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit mollitia debitis, quod</p>
+          </Card>
+          <Card className={"w-96 p-6 mt-4 bg-white shadow-md"}>
+            <h1>
+              Produto 2
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit mollitia debitis, quod</p>
+          </Card>
+          <Card className={"w-96 p-6 mt-4 bg-white shadow-md"}>
+            <h1>
+              Produto 3
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit mollitia debitis, quod</p>
+          </Card>
+        </div>
+      </div>
     </>
   );
 }
